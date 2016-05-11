3c211266b5fbeb891da77fd0a8ed2037e18c7e08
shahryarjb <shahryar@shahryars-MacBook-Air.local>
2016-05-11 18:02:20 +0430

    add new router and lang

--- site/router.php
+++ site/router.php
@@ -6,28 +6,62 @@ class MiniuniversityRouter
 {
 	public function build(&$query) {
 		$segments = array();
-       
-       if (isset($query['id']))
-       {
-                $segments[] = $query['id'];
-                unset($query['id']);
-       };
-       unset($query['view']);
-       return $segments;
+		
+			
+		$menu    =   &JSite::getMenu();
+		$id    =   $menu->getActive()->id;
+
+		$db= JFactory::getDbo();
+		$dbquery = $db->getQuery(true);
+		
+
+		$dbquery->select('id');
+		$dbquery->from($db->quoteName('#__menu'));
+		$like = ('%' . $db->escape( 'view=search', true ) . '%' );
+		$dbquery->where('link LIKE' . $db->quote( $like, false));
+ 
+		$db->setQuery($dbquery);
+		$searchId = $db->loadResult();
+		
+		if (($query['view'] == 'search') && (count($query) == 3)) 
+		
+		{
+			$segments [] = $query['view'];	 
+		}
+		
+		else if ($id == $searchId) {
+			
+			$segments [] = $query['view'];	 
+		}
+	
+		if (isset($query['id'])) 
+		{
+			$segments [] = $query['id'];
+			unset ($query['id']);
+		}
+		
+	   unset ($query['view']);
+		return $segments;
 	}
-//--------------------------------------------------------------------------------------
+	
 public function parse(&$segments) {
-	  $vars = array();
+
+	$vars = array();
        $app = JFactory::getApplication();
        $menu = $app->getMenu();
        $item = $menu->getActive();
        // Count segments
        $count = count($segments);
        // Handle View and Identifier
-       switch ($item->query['view']) {
+
+	   switch ($item->query['view']) {
+		
 			case 'search':
 			{
-				$vars['view'] = 'search';
+				
+				//$vars['view'] = 'search'; // 
+				
+				
 				break;
 			}
 			
@@ -60,19 +94,53 @@ public function parse(&$segments) {
 				break;
 			}
 		}
-       return $vars;
+	
+			
+		switch($segments[0])
+		{
+			
+			case 'search':
+				{
+				$vars['view']= 'search'; 
+				
+				break;
+				}
+			
+			case 'card':
+				{
+				$vars['view']= 'card';
+				break;
+				}
+				
+			case 'teacher':
+				{
+				$vars['view']= 'teacher';
+				$id = explode(':', $segments[1]);
+				$vars['id'] = (int) $id[0];
+				break;
+				}
+			case 'lib':
+				{
+				$vars['view']= 'lib';
+				$id = explode(':', $segments[1]);
+				$vars['id'] = (int) $id[0];
+				break;
+				}
+				
+		}  
+	return $vars;
 	}
+
 }
-//=========================================================================================
-	function miniuniversityBuildRoute(&$query) {
-		$router = new MiniuniversityRouter;
 
-		return $router->build($query);
-	}
-	//-------------------------------------------------------------
-	function miniuniversityParseRoute($segments) {
-		$router = new MiniuniversityRouter;
+function miniuniversityBuildRoute(&$query) {
+$router = new MiniuniversityRouter;
 
-		return $router->parse($segments);
-	}
+	return $router->build($query);
+}
 
+function miniuniversityParseRoute($segments) {
+	$router = new MiniuniversityRouter;
+
+	return $router->parse($segments);
+}
